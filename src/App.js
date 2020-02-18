9b48db5f15211d342964d547ae5a81cdb59b18c7
romyaudio <romyaudio@hotmail.com>
2020-02-17 23:00:32 -0500

    mi primera modificacion

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Mylogo from './img/ticketssy-icon.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={Mylogo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> ticketssy-react.
         </p>
