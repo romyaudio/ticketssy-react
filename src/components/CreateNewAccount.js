b11387a825149a591eda686ac6bfe055a58571b7
romyaudio <romyaudio@hotmail.com>
2020-03-01 15:05:07 -0500

    build test

---
+++ src/components/CreateNewAccount.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const CreateNewAccount = () => (
+	<div>
+		<div>
+			<h1>Home</h1>
+		</div>
+		<Link to="Dashboard">Create an Account</Link>
+
+	</div>
+)
+export default CreateNewAccount
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import HOME from '../components/CreateNewAccount'
+
+class Home extends React.Component{
+    render(){
+	return(
+        <HOME/>
+
+		)
+	}
+}
+export default Home
