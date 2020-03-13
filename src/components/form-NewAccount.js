243b0bce80fd1763370e0b27c925d6679839342f
romyaudio <romyaudio@hotmail.com>
2020-03-13 19:16:56 -0400

    building

--- src/components/form-NewAccount.js
+++ src/components/form-NewAccount.js
@@ -1,44 +1,46 @@
 import React from 'react'
+import './styles/form-NewAccount.css'
 
-const formNewAccount = ({onChange,onSubmit,form}) =>(
-
+const formNewAccount = ({onChange,onSubmit,form,errors}) =>(
 	<div className="container">
 	<form onSubmit={onSubmit}>
-       
 	<div className="form-group">
 	<label>Full name</label>
 	<input
-	 type="text" 
-	 className="form-control" 
+	 type="text"
+	 className="form-control"
 	 name="name"
 	 aria-describedby="emailHelp"
 	 onChange={onChange}
 	 value={form.name}
 	 />
+	 <small>{errors.name}</small>
 	</div>
 
 	<div className="form-group">
 	<label>Email address</label>
 	<input
-	 type="email" 
-	 className="form-control" 
-	 name="email" 
+	 type="email"
+	 className="form-control"
+	 name="email"
 	 aria-describedby="emailHelp"
 	 onChange={onChange}
 	 value={form.email}
 	 />
+	 <small>{errors.email}</small>
 	<small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
 	</div>
 
 	<div className="form-group">
 	<label>Password</label>
 	<input
-	 type="password" 
+	 type="password"
 	 className="form-control"
 	 name="password"
 	 onChange={onChange}
 	 value={form.password}
 	 />
+	 <small>{errors.password}</small>
 	</div>
 
 	<div className="form-group">
@@ -46,12 +48,12 @@ const formNewAccount = ({onChange,onSubmit,form}) =>(
 	<input
 	 type="password"
 	 className="form-control"
-	 name="confiPassword"
+	 name="password_confirmation"
 	 onChange={onChange}
-	 value={form.confiPassword}
+	 value={form.password_confirmation}
 	 />
 	</div>
-	
+
 	<button type="submit" className="btn btn-primary">Submit</button>
 
 	</form>
