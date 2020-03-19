fb34c9ea9e97a4a11aa3e43782818da9fe6e3ffa
romyaudio <romyaudio@hotmail.com>
2020-03-18 20:07:26 -0400

    building

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,76 +2,75 @@ import React from 'react'
 import Header from '../components/Header'
 import axios from 'axios'
 import ConDashboard from '../components/Dashboard'
+axios.defaults.baseURL = 'http://localhost:8000'
 
 class Dashboard extends React.Component {
 
-    
-     state = {
-     	name:""
-     }
-     componentDidMount() {
-        if (localStorage.getItem('token') === "") {
+
+   state = {
+      name:""
+  }
+  componentDidMount() {
+    if (localStorage.getItem('token') === "") {
         this.props.history.push('login');
     }
 
-     	let token = localStorage.getItem("token")
-     	axios({
-     		method: 'get',
-     		url: '/api/user',
-     		headers:{
-     			Authorization:`Bearer ${token}`,
-     		}
-     	})
-     	.then(response =>{
-     		this.setState({
-     			name: response.data.name
-     		})
-     	})
-
-        .catch(error=>{
-            this.props.history.push('login');
-            localStorage.setItem('token','')
-        })
-
-
-     }
-
-		hendleClick = e =>{
-			axios({
-     		method: 'get',
-     		url: '/api/logout',
-     		headers:{
-     			Authorization:`Bearer ${localStorage.getItem("token")}`,
-     		}
-     	})
-			.then(response =>{
-				console.log(response)
-				this.props.history.push('login');
-				localStorage.setItem('token','')
-			})
-
-            .catch(error=>{
-                this.props.history.push('login');
-                localStorage.setItem('token','')
-            })
-		}
-
-
-
-    render(){
-    	return(
-    		<div>
-              <Header
-              onClick={this.hendleClick}
-              buss={this.state.name}
-              />
-              <div>
-              <ConDashboard
-              />
-              </div>
-            </div>
-
-    		)
+    let token = localStorage.getItem("token")
+    axios({
+     method: 'get',
+     url: '/api/user',
+     headers:{
+        Authorization:`Bearer ${token}`,
     }
+})
+    .then(response =>{
+     this.setState({
+        name: response.data.name
+    })
+ })
+
+    .catch(error=>{
+        this.props.history.push('login');
+        localStorage.setItem('token','')
+    })
+}
+
+hendleClick = e =>{
+ axios({
+     method: 'get',
+     url: '/api/logout',
+     headers:{
+        Authorization:`Bearer ${localStorage.getItem("token")}`,
+    }
+})
+ .then(response =>{
+    console.log(response)
+    this.props.history.push('login');
+    localStorage.setItem('token','')
+})
+
+ .catch(error=>{
+    this.props.history.push('login');
+    localStorage.setItem('token','')
+})
+}
+
+
+
+render(){
+   return(
+      <div>
+      <Header
+      onClick={this.hendleClick}
+      buss={this.state.name}
+      />
+      <div>
+      <ConDashboard
+      />
+      </div>
+      </div>
+
+      )
+}
 }
 export default Dashboard
--- src/pages/Reset-Password.js
+++ src/pages/Reset-Password.js
@@ -20,6 +20,21 @@ class ResetPassword extends React.Component{
 
 	hendleSubmit = async e =>{
 		e.preventDefault()
+		try{
+			let config = {
+				method:'POST',
+				headers:{
+					'Accept':'application/json',
+					'Content-Type':'application/json'
+				},
+				body: JSON.stringify(this.state.form)
+			}
+
+			let res = await fetch('http://localhost:8000/api/forgotpassword',config);
+			let json = await res.json();
+		}catch(error){
+
+		}
 	}
 	render(){
 		const {form} = this.state
