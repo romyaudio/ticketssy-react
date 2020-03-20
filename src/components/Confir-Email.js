a727c7b1e537ed38d5d9ec37347d3b1466762d59
romyaudio <romyaudio@hotmail.com>
2020-03-19 23:47:41 -0400

    building

--- src/components/Confir-Email.js
+++ src/components/Confir-Email.js
@@ -3,11 +3,13 @@ import React from 'react'
 const ConfirEmail = ({form}) =>(
 
 	<div className="jumbotron">
+	<div className="container p-3 bg-light">
 	<h1 className="display-4">Congratulation!</h1>
 	<p className="lead">Dear: {form.name}, your account was successfully created.</p>
 	<hr className="my-4"/>
-	<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+	<p>We have sent a link to your email. <strong>{form.email}</strong> To verify your email.</p>
 	<a className="btn btn-primary btn-lg" href="login" role="button">Login</a>
+	</div>
 	</div>
 	)
 	export default ConfirEmail
--- src/components/NewPassword.js
+++ src/components/NewPassword.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 const NewPassword = ({email}) =>(
 
 	<div className="jumbotron">
-	  <div className="container p-3 mb-2 bg-info text-white">
+	  <div className="container p-3 mb-2 bg-light">
 		<h4 className="display-4">Reset Password!</h4>
 			<p className="lead">We have sent a link to your email. <strong>{email}</strong> to reset your password.</p>
 			 <hr clasNames="my-4"/>
